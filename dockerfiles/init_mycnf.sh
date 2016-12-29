12bc294b076f4fe7aa33181394c0e805c2da7332
刘金烨 <75710541@qq.com>
2016-12-29 14:01:35 +0800

    Update init_mycnf.sh

--- dockerfiles/init_mycnf.sh
+++ dockerfiles/init_mycnf.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-num=`hostname -s|grep -o '[[:digit:]]*'|tail -1`
+num=`echo $HOSTNAME|grep -o '[[:digit:]]*'|tail -1`
 if [ "$num" ];then
     sed -i 's/server-id[[:space:]]*=[[:space:]]*[[:digit:]]*[[:space:]]*/server-id='$num'/g' /etc/my.cnf
 fi
